a1026636da5d1ed2d15eb59a79a48cb2c3456bc9
aabir2011 <80570715+aabir2011@users.noreply.github.com>
2021-06-09 17:58:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Project 62-Avengers</title>
+    <link rel="icon" href="https://c4.wallpaperflare.com/wallpaper/865/653/298/2012-the-avengers-avengers-logo-wallpaper-preview.jpg">
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="line"></div>
+    <br>
+    <section id="main">
+        <img height="100" src="https://c4.wallpaperflare.com/wallpaper/865/653/298/2012-the-avengers-avengers-logo-wallpaper-preview.jpg">
+    </section>
+    <section id="secondmain">
+        <img height="100" src="avenge.png">
+    </section>
+    <br>
+    <div class="line"></div>
+    <section>
+        <h1>Grid gallery</h1>
+        <div class="red">
+            <br>
+            <div>
+                <span><img height="200" width="200" src="https://pyxis.nymag.com/v1/imgs/8fa/98e/3a01cd0da4f1558b478d5bf7a74e488f1d-05-evans-america.rsquare.w330.jpg"></span>
+                <span><img height="200" width="200" src="iron%20man.jpg"></span>
+                <span><img height="200" width="200" src="https://pyxis.nymag.com/v1/imgs/743/05c/c32caeb780ba75be7f82725d7ac5d23ab7-30-thor.rsquare.w700.jpg"></span>
+                <span><img height="200" width="200" src="https://i.pinimg.com/originals/98/3b/12/983b12b40523d8d24fc82bafb0f02da7.jpg"></span>
+            </div>
+            <br>
+            <div>
+                <span><img height="200" width="200" src="https://pyxis.nymag.com/v1/imgs/7b1/bf8/e1b99f5d9939bde8387c66daf15f39a2af-10-scarjo-black-widow.rsquare.w330.jpg"></span>
+                <span><img height="200" width="200" src="hawk%20eye.jpg"></span>
+                <span><img height="200" width="200" src="https://www.femalefirst.co.uk/image-library/square/1000/c/civil-war-promo-art-scarlet-witch-1.jpg"></span>
+                <span><img height="200" width="200" src="https://images.popbuzz.com/images/7371?crop=16_9&width=660&relax=1&signature=zMKvXHA3ggIxuAD3eNT0WjgNT98="></span>
+            </div>
+            <br>
+            <div>
+                <span><img height="200" width="200" src="https://www.pngitem.com/pimgs/m/160-1601335_transparent-vision-marvel-png-vision-marvel-civil-war.png"></span>
+                <span><img height="200" width="200" src="https://pyxis.nymag.com/v1/imgs/288/3b0/e54e4bfd4095f34eca92495317353abcb8-23-doctor-strange.rsquare.w700.jpg"></span>
+                <span><img height="200" width="200" src="https://www.ginx.tv/uploads/Banner_images/Fortnite_Chapter_2_Season_2_Skins/Marvel_Avengers/Chadwick_Marvel_s_Avengers.png"></span>
+                <span><img height="200" width="200" src="https://i.pinimg.com/564x/cc/a5/0a/cca50ac12dd67dc6e64d021abc403be2.jpg"></span>
+            </div>
+            <br>
+        </div>
+    </section>
+    <section class="container">
+        <h1>Sliding gallery</h1>
+        <div id="mycarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#mycarousel" data-slide-to="1"></li>
+                <li data-target="#mycarousel" data-slide-to="2"></li>
+                <li data-target="#mycarousel" data-slide-to="3"></li>
+                <li data-target="#mycarousel" data-slide-to="4"></li>
+                <li data-target="#mycarousel" data-slide-to="5"></li>
+                <li data-target="#mycarousel" data-slide-to="6"></li>
+                <li data-target="#mycarousel" data-slide-to="7"></li>
+                <li data-target="#mycarousel" data-slide-to="8"></li>
+                <li data-target="#mycarousel" data-slide-to="9"></li>
+                <li data-target="#mycarousel" data-slide-to="10"></li>
+                <li data-target="#mycarousel" data-slide-to="11"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img width="200" src="https://pyxis.nymag.com/v1/imgs/8fa/98e/3a01cd0da4f1558b478d5bf7a74e488f1d-05-evans-america.rsquare.w330.jpg">
+                    <div class="carousel-content" id="ca">
+                        <p>Captain America</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="iron%20man.jpg">
+                    <div class="carousel-content" id="im">
+                        <p>Iron Man</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="https://pyxis.nymag.com/v1/imgs/743/05c/c32caeb780ba75be7f82725d7ac5d23ab7-30-thor.rsquare.w700.jpg">
+                    <div class="carousel-content" id="thor">
+                        <p>Thor</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="https://i.pinimg.com/originals/98/3b/12/983b12b40523d8d24fc82bafb0f02da7.jpg">
+                    <div class="carousel-content" id="hulk">
+                        <p>Bruce Banner/Hulk</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="https://pyxis.nymag.com/v1/imgs/7b1/bf8/e1b99f5d9939bde8387c66daf15f39a2af-10-scarjo-black-widow.rsquare.w330.jpg">
+                    <div class="carousel-content" id="widow">
+                        <p>Black widow</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="hawk%20eye.jpg">
+                    <div class="carousel-content" id="hawk">
+                        <p>Hawk eye</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="https://www.femalefirst.co.uk/image-library/square/1000/c/civil-war-promo-art-scarlet-witch-1.jpg">
+                    <div class="carousel-content" id="witch">
+                        <p>Scarlet witch</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="https://images.popbuzz.com/images/7371?crop=16_9&width=660&relax=1&signature=zMKvXHA3ggIxuAD3eNT0WjgNT98=">
+                    <div class="carousel-content" id="silver">
+                        <p>Quicksilver</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="https://www.pngitem.com/pimgs/m/160-1601335_transparent-vision-marvel-png-vision-marvel-civil-war.png">
+                    <div class="carousel-content" id="see">
+                        <p>Vision</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="https://pyxis.nymag.com/v1/imgs/288/3b0/e54e4bfd4095f34eca92495317353abcb8-23-doctor-strange.rsquare.w700.jpg">
+                    <div class="carousel-content" id="weird">
+                        <p>Doctor Strange</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="https://www.ginx.tv/uploads/Banner_images/Fortnite_Chapter_2_Season_2_Skins/Marvel_Avengers/Chadwick_Marvel_s_Avengers.png">
+                    <div class="carousel-content" id="bp">
+                        <p>Black Panther</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="200" src="https://i.pinimg.com/564x/cc/a5/0a/cca50ac12dd67dc6e64d021abc403be2.jpg">
+                    <div class="carousel-content">
+                        <p>Spider Man</p>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <a class="left carousel-control" href="#mycarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+            <a class="right carousel-control" href="#mycarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div>
+        <br>
+        <br>
+        <br>
+    </section>
+</body></html>
